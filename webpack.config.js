80cacdf9ebab2dba5c4f28d60355dfd8b6cf9617
dasari234 <dasari9@gmail.com>
2018-12-28 11:20:51 +0530

    setup dev dependecy

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
   const isProduction = env === 'production';
@@ -9,7 +8,7 @@ module.exports = (env) => {
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: {
@@ -22,15 +21,15 @@ module.exports = (env) => {
         use: CSSExtract.extract({
           use: [
             {
-              loader:'css-loader',
-              options:{
-                sourceMap:true
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
               }
             },
             {
-              loader:'sass-loader',
-              options:{
-                sourceMap:true
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
               }
             }
           ]
@@ -43,7 +42,8 @@ module.exports = (env) => {
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
-      historyApiFallback: true
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
-  }
-}
+  };
+};
